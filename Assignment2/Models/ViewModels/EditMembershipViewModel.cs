69dab66b0a5fa81e5ec962632df7152a3da63c2a
JosefKundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-08-08 16:22:56 -0400

    Edit memberhsip complete

---
+++ Assignment2/Models/ViewModels/EditMembershipViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models.ViewModels
+{
+    public class EditMembershipViewModel
+    {
+        public IEnumerable<Community> Communities { get; set; }
+
+        public IEnumerable<CommunityMembership> CommunityMemberships { get; set; }
+
+        public Student Student { get; set; }
+    }
+}
